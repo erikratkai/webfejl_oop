56141dadc628b7b2779f7cc3eacdf1776723dd89
Ratkai Erik <ratkai.erik@BOLYAI.LOCAL>
2024-12-13 15:04:39 +0100

    hejezsirazs

---
+++ table/scripts.js
@@ -0,0 +1,60 @@
+const array = [
+    {
+        firstname1: 'Géza',
+        firstname2: 'Ferenc',
+        lastname: 'Kocsis',
+    },
+    {
+        firstname1: 'Mária',
+        firstname2: 'Júlia',
+        lastname: 'Horváth',
+    },
+    {
+        firstname1: 'Ferenc',
+        lastname: 'Balogh',
+    },
+    {
+        firstname1: 'Gábor',
+        firstname2: 'Attila',
+        lastname: 'Horváth'
+    },
+]
+
+class Person{
+    constructor(obj){
+        this.firstname1 = obj.firstname1;
+        this.firstname2 = obj.firstname2;
+        this.lastname = obj.lastname
+    }
+    render(parentElement){
+        const sor = document.createElement('tr')
+        parentElement.appendChild(sor)
+
+        const td = document.createElement('td')
+        td.innerHTML = this.lastname
+        sor.appendChild(td)
+
+        const td1 = document.createElement('td')
+        td1.innerHTML = this.firstname1  
+        sor.appendChild(td1)
+        
+        if (this.firstname2 !== undefined){
+            const td2 = document.createElement('td')
+            td2.innerHTML = this.firstname2
+            sor.appendChild(td2)
+        } 
+        else{
+            td1.colSpan = 2; 
+        }
+        //nem html elementeknek nem lehet a colspanja 2; csak html elementnek, es ha a firstname2 undefined, akkor, hogy ne maradjon üresen a cella a td1 colspanja lesz 2.
+    }
+}
+
+function init(){
+    for(let i = 0; i < array.length; i++){
+        const pers = new Person(array[i])
+        pers.render(document.getElementById('tbodyId'));
+    }
+}
+
+init();
